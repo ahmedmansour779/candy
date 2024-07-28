5ce89b15e97b05f0d63927dfe50ea3a2586b75b3
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-07-29 01:21:32 +0300

    test vercel

--- src/components/cards/CardUpload.tsx
+++ src/components/cards/CardUpload.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Typography, Progress } from "antd";
 import { CheckCircleFilled } from "@ant-design/icons";
-import { AppDispatch, RootState } from "../../store/store";
+import { Progress, Typography } from "antd";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCatchFile } from "../../store/slices/GlobalSlice";
+import { AppDispatch, RootState } from "../../store/store";
 
 const { Text } = Typography;
 
@@ -18,26 +18,14 @@ interface Props {
 export default function CardUpload({ text, icon, dataSize, filesType }: Props) {
   const { catchFile } = useSelector((state: RootState) => state.GlobalReducer);
   const dispatch: AppDispatch = useDispatch();
-<<<<<<< HEAD
-  
-  const [loading, setLoading] = useState<boolean>(false);
-  const [progress, setProgress] = useState<number>(0);
-  
-=======
 
   const [loading, setLoading] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
 
->>>>>>> 4433471fa573c0c9309f507703f871259ecacb88
   useEffect(() => {
     if (catchFile) {
       setLoading(true);
       setProgress(0);
-<<<<<<< HEAD
-      
-=======
-
->>>>>>> 4433471fa573c0c9309f507703f871259ecacb88
       const interval = setInterval(() => {
         setProgress((oldProgress) => {
           if (oldProgress === 100) {
@@ -54,11 +42,8 @@ export default function CardUpload({ text, icon, dataSize, filesType }: Props) {
 
   const isFileTypeMatched = filesType.some((item) => text.toString().toLowerCase().includes(item));
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 4433471fa573c0c9309f507703f871259ecacb88
   return (
     <div className="px-6 py-4 flex items-center justify-between card">
       <div className="flex-center gap-4 h-fit">
@@ -74,24 +59,21 @@ export default function CardUpload({ text, icon, dataSize, filesType }: Props) {
           <Text className="text-gray-500 text-xs">{dataSize} MB</Text>
         </div>
       </div>
-<<<<<<< HEAD
       {loading && isFileTypeMatched ? (
         <Progress percent={progress} status="active" style={{ width: 120 }} />
       ) : (
         <CheckCircleFilled className="text-xl text-[#27C72B]" />
       )}
-=======
-      {loading && isFileTypeMatched && text.toLowerCase() !== "documents"  ? (
+      {loading && isFileTypeMatched && text.toLowerCase() !== "documents" ? (
         <Progress percent={progress} status="active" style={{ width: 120 }} />
       ) :
-        loading &&  text.toLowerCase() == "documents"  ?
+        loading && text.toLowerCase() == "documents" ?
           (
             <Progress percent={progress} status="active" style={{ width: 120 }} />
           ) :
           (
             <CheckCircleFilled className="text-xl text-[#27C72B]" />
           )}
->>>>>>> 4433471fa573c0c9309f507703f871259ecacb88
     </div>
   );
 }
